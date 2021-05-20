088f43de35fae5336b5e16fd801f5444eddcc2dc
PVSAKHIL <63859299+PVSAKHIL@users.noreply.github.com>
2021-05-20 19:16:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>my favourite food</h1>
+    <ul>
+      <li>bacon</li>
+      <li>burger</li>
+      <li></li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
